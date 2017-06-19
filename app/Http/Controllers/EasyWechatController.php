46d92e627127ab2dec335b358c6e2f9ed7908a54
洗章耀 <xizy@xizhangyaodeMacBook-Pro.local>
2017-06-19 15:52:59 +0800

    wechat

--- app/Http/Controllers/EasyWechatController.php
+++ app/Http/Controllers/EasyWechatController.php
@@ -30,37 +30,58 @@ class EasyWechatController extends Controller
     public function verify()
     {
 
-        $server = $this->app->server;
+//        $server = $this->app->server;
+//        $server->setMessageHandler(function ($message) {
+//            switch ($message->MsgType) {
+//                case 'event':
+//                    return '收到事件消息';
+//                    break;
+//                case 'text':
+//                    return '收到文字消息';
+//                    break;
+//                case 'image':
+//                    return '收到图片消息';
+//                    break;
+//                case 'voice':
+//                    return '收到语音消息';
+//                    break;
+//                case 'video':
+//                    return '收到视频消息';
+//                    break;
+//                case 'location':
+//                    return '收到坐标消息';
+//                    break;
+//                case 'link':
+//                    return '收到链接消息';
+//                    break;
+//                // ... 其它消息
+//                default:
+//                    return '收到其它消息';
+//                    break;
+//            }
+//        });
+//        $response=$server->serve();
+//        return $response;
+        $options = [
+            'debug'  => false,
+            'app_id' => env('WECHAT_APP_ID'),
+            'secret' => env('WECHAT_APP_SECRET'),
+            'token'  => env('WECHAT_TOKEN'),
+            // 'aes_key' => null, // 可选
+            'log' => [
+                'level' => 'debug',
+                'file'  => '/tmp/easywechat.log', // XXX: 绝对路径！！！！
+            ],
+
+        ];
+        $app = new Application($options);
+        $server = $app->server;
         $server->setMessageHandler(function ($message) {
-            switch ($message->MsgType) {
-                case 'event':
-                    return '收到事件消息';
-                    break;
-                case 'text':
-                    return '收到文字消息';
-                    break;
-                case 'image':
-                    return '收到图片消息';
-                    break;
-                case 'voice':
-                    return '收到语音消息';
-                    break;
-                case 'video':
-                    return '收到视频消息';
-                    break;
-                case 'location':
-                    return '收到坐标消息';
-                    break;
-                case 'link':
-                    return '收到链接消息';
-                    break;
-                // ... 其它消息
-                default:
-                    return '收到其它消息';
-                    break;
-            }
+            // $message->FromUserName // 用户的 openid
+            // $message->MsgType // 消息类型：event, text....
+            return "您好！欢迎关注我!";
         });
-        $response=$server->serve();
+        $response = $server->serve();
         return $response;
     }
 
