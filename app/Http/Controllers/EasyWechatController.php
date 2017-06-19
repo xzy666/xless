ef3566d4d57d74daf3e1423dfb9ac4e17c232d90
洗章耀 <xizy@xizhangyaodeMacBook-Pro.local>
2017-06-19 16:38:00 +0800

    wechat

--- app/Http/Controllers/EasyWechatController.php
+++ app/Http/Controllers/EasyWechatController.php
@@ -46,11 +46,11 @@ class EasyWechatController extends Controller
         $server = $app->server;
         $user = $app->user;
 
-        $server->setMessageHandler(function($message) use ($user) {
-            $fromUser = $user->get($message->FromUserName);
-
-            return "{$fromUser->nickname} 您好！欢迎关注 xizy!";
-        });
+//        $server->setMessageHandler(function($message) use ($user) {
+//            $fromUser = $user->get($message->FromUserName);
+//
+//            return "{$fromUser->nickname} 您好！欢迎关注 xizy!";
+//        });
 
         return $server->serve();
     }
